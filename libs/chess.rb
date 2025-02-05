b57dafc5e98dab043b80748a8883f7e26f8eeff7
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2025-02-05 20:41:14 +0200

    created the pieces class and function for their placement in th board

--- libs/chess.rb
+++ libs/chess.rb
@@ -1,3 +1,5 @@
+require_relative './pieces'
+
 class Board
   attr_accessor :board
 
@@ -16,8 +18,36 @@ class Board
     end
   end
 
+  def piece_placement_board()
+    # Black pieces
+    @board[0][0] = Piece.new(:black, :rook)
+    @board[0][1] = Piece.new(:black, :knight)
+    @board[0][2] = Piece.new(:black, :bishop)
+    @board[0][3] = Piece.new(:black, :queen)
+    @board[0][4] = Piece.new(:black, :king)
+    @board[0][5] = Piece.new(:black, :bishop)
+    @board[0][6] = Piece.new(:black, :knight)
+    @board[0][7] = Piece.new(:black, :rook)
+
+    # Black pawns
+    (0..7).each { |col| @board[1][col] = Piece.new(:black, :pawn) }
+
+    # White pieces
+    @board[7][0] = Piece.new(:white, :rook)
+    @board[7][1] = Piece.new(:white, :knight)
+    @board[7][2] = Piece.new(:white, :bishop)
+    @board[7][3] = Piece.new(:white, :queen)
+    @board[7][4] = Piece.new(:white, :king)
+    @board[7][5] = Piece.new(:white, :bishop)
+    @board[7][6] = Piece.new(:white, :knight)
+    @board[7][7] = Piece.new(:white, :rook)
+
+    # White pawns
+    (0..7).each { |col| @board[6][col] = Piece.new(:white, :pawn) }
+  end
 
 end
 
 board = Board.new
+board.piece_placement_board
 board.display_board
